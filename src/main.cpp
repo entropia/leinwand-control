6ae6c4df8a0d7535142bbc5ba458eaa2d4a04839
GityUpNow <kim-cheng@gmx.de>
2017-11-27 20:30:29 +0100

    Did some cleanups and removed a bit of unnecessary code

--- src/main.cpp
+++ src/main.cpp
@@ -11,42 +11,50 @@
 #include <Adafruit_MQTT.h>
 #include <Adafruit_MQTT_Client.h>
 
-#include "Wifi_credentials.hpp"
-#include "mqtt_credentials.hpp"
+#include "WiFi_credentials.hpp"
+#include "MQTT_credentials.hpp"
 
+//Object declarations
 Adafruit_BME280 bme;
 WiFiClient client;
 Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_SERVER_PORT, MQTT_USER, MQTT_PASSWORD);
 
 //Functions prototypes
 void setupWiFi();
-bool readBME(float *bmeData);
-void MQTT_connect();
+void readBME(float *bmeData);
+void mqtt_connect();
 void mqtt_publish(float *bmeData);
 
 void setup() {
   Serial.begin(115200);
-  Serial.println(F("BMP280 test"));
-
   setupWiFi();
-  bme.begin(0x76);
-  delay(420);
+
+
+  if(!bme.begin(0x76))
+    Serial.println(F("Could not find a BME280 Sensor!"));
+
+  delay(400);
 }
 
 
 void loop() {
     float bmeData[3];
-    bool bmesuccess = readBME(bmeData);
-    MQTT_connect();
+    readBME(bmeData);
+
+    mqtt_connect();
+    mqtt_publish(bmedata);
+
+    //Record data every 15 seconds
     delay(15000);
-    mqtt_publish(bmeData);
 }
 
+
+//Function definitions
 void setupWiFi() {
   WiFi.mode(WIFI_STA);
 
   while ( WiFi.status() != WL_CONNECTED) {
-    Serial.print("Attempting to connect network, SSID: ");
+    Serial.print(F("Attempting to connect network, SSID: "));
     Serial.println(SSID);
     WiFi.begin(SSID, WIFIPW);
 
@@ -56,43 +64,46 @@ void setupWiFi() {
     }
   }
 
-  Serial.printf("\nWifi Conected! \\o/ \n IP Address:");
+  Serial.printf(F("\nWifi Conected! \\o/ \n IP Address:"));
   Serial.println(WiFi.localIP());
 }
 
-bool readBME(float* bmeData) {
+
+void readBME(float* bmeData) {
   float T = bme.readTemperature();
   delay(50);
+
   float P = bme.readPressure();
   delay(50);
+
   float H = bme.readHumidity();
   delay(50);
+
   bmeData[0] = T;
   bmeData[1] = P;
   bmeData[2] = H;
-  return 1;
 }
 
-void MQTT_connect() {
-  int8_t ret;
-
+void mqtt_connect() {
   // Stop if already connected.
-  if (mqtt.connected()) {
+  if(mqtt.connected()){
     return;
   }
 
   Serial.print(F("Connecting to MQTT... "));
 
+  int8_t ret;
   uint8_t retries = 3;
-  while ((ret = mqtt.connect()) != 0) { // connect will return 0 for connected
+  while((ret = mqtt.connect()) != 0){ // connect will return 0 for connected
        Serial.println(mqtt.connectErrorString(ret));
        Serial.println(F("Retrying MQTT connection in 5 seconds..."));
+
        mqtt.disconnect();
        delay(5000);  // wait 5 seconds
+
        retries--;
-       if (retries == 0) {
-         // basically die and wait for WDT to reset me
-         while (1);
+       if(retries == 0){
+         while (1); // basically die and wait for WDT to reset me
        }
   }
   Serial.println(F("MQTT Connected!"));
@@ -101,8 +112,10 @@ void MQTT_connect() {
 void mqtt_publish(float *bmeData) {
   Adafruit_MQTT_Publish temperature = Adafruit_MQTT_Publish(&mqtt, MQTT_TOPIC "/T");
   temperature.publish(bmeData[0]);
+
   Adafruit_MQTT_Publish pressure = Adafruit_MQTT_Publish(&mqtt, MQTT_TOPIC "/P");
   pressure.publish(bmeData[1]);
+
   Adafruit_MQTT_Publish humidity = Adafruit_MQTT_Publish(&mqtt, MQTT_TOPIC "/H");
   humidity.publish(bmeData[2]);
 }
