7226540449fc8d3681e07701a10f8fc220dba2dd
GityUpNow <kim-cheng@gmx.de>
2018-01-01 23:14:10 +0100

    Replaced the main.cpp with my modified version

--- src/main.cpp
+++ src/main.cpp
@@ -15,16 +15,31 @@
 #include "WiFi_credentials.hpp"
 #include "MQTT_credentials.hpp"
 
+//Global Constants
+#define MEASURE 15000
+#define PULSE 50
+
+#define DOWN_PIN 13
+#define STOP_PIN 12
+#define   UP_PIN 14
+#define MISC_PIN 16
+
 //Object declarations
 Adafruit_BME280 bme;
 WiFiClient client;
 Adafruit_MQTT_Client mqtt(&client, MQTT_SERVER, MQTT_SERVER_PORT, MQTT_USER, MQTT_PASSWORD);
+Adafruit_MQTT_Subscribe control(&mqtt, MQTT_CONTROL_TOPIC);
+
+//Global Variables
+unsigned long last = 0;
 
 //Functions prototypes
+void pulse(byte pin);
 void setupWiFi();
 void readBME(float *bmeData);
 void mqtt_connect();
 void mqtt_publish(float *bmeData);
+void mqtt_callback(char *str, uint16_t len);
 
 void setup() {
   Serial.begin(115200);
@@ -34,18 +49,29 @@ void setup() {
     Serial.println(F("Could not find a BME280 Sensor!"));
 
   delay(400);
+
+  //Subscribe to the Leinwand-Controlfeed
+  control.setCallback(mqtt_callback);
+  mqtt.subscribe(&control);
+  
+  //Pin Setups
+  pinMode(DOWN_PIN, OUTPUT);
+  pinMode(STOP_PIN, OUTPUT);
+  pinMode(UP_PIN, OUTPUT);
+  pinMode(MISC_PIN, OUTPUT);
 }
 
 
 void loop() {
+    mqtt_connect();    
+
     float bmeData[3];
     readBME(bmeData);
 
-    mqtt_connect();
     mqtt_publish(bmeData);
+    last = millis();
 
-    //Record data every 15 seconds
-    delay(15000);
+    mqtt.processPackets(MEASURE);
 }
 
 
@@ -90,8 +116,6 @@ void mqtt_connect() {
     return;
   }
 
-  Serial.print(F("Connecting to MQTT... "));
-
   int8_t ret;
   uint8_t retries = 3;
   while((ret = mqtt.connect()) != 0){ // connect will return 0 for connected
@@ -118,4 +142,34 @@ void mqtt_publish(float *bmeData) {
 
   Adafruit_MQTT_Publish humidity = Adafruit_MQTT_Publish(&mqtt, MQTT_TOPIC "/H");
   humidity.publish(bmeData[2]);
+}
+
+
+void mqtt_callback(char *str, uint16_t len) {
+  if(len == 1) {
+    char control = *str;
+    
+    Serial.println(control);
+
+    switch(control) {
+	case('u'):{
+          pulse(UP_PIN);    
+        }break;
+        case('s'):{
+          pulse(STOP_PIN);    
+        }break;
+        case('d'):{
+          pulse(DOWN_PIN);    
+	}break;
+	case('m'):{
+          pulse(MISC_PIN);    
+	}break;
+    }
+  }
+}
+
+void pulse(byte pin){
+  digitalWrite(pin, HIGH);
+  delay(PULSE);
+  digitalWrite(pin, LOW);
 }
